1703c3f81dd270a931c2c17595fc4a55d232cea7
임준영 <a79007714@gmail.com>
2022-09-20 23:25:02 +0900

    kotlin in action Exam08_1 고차함수 예제코드 작성

---
+++ src/chapter08/Exam08_1.kt
@@ -0,0 +1,20 @@
+package chapter08
+
+import java.lang.StringBuilder
+
+fun String.filter(predicate: (Char) -> Boolean): String {
+
+    val sb = StringBuilder()
+
+    for (index in 0 until length) {
+        val element = get(index)
+        println("element = $element")
+        if (predicate(element)) sb.append(element)
+    }
+
+    return sb.toString()
+}
+
+fun main(args: Array<String>) {
+   println("ab1c".filter { it in 'a'..'z' })
+}
