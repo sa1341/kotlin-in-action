5ff8af353d14481719bbc3c642f25751ec404552
sa1341 <a79007714@gmail.com>
2024-02-29 22:29:10 +0900

    chore: ktlint 적용

--- src/fp/chapter3/excercise2/Exercise.kt
+++ src/fp/chapter3/excercise2/Exercise.kt
@@ -91,24 +91,28 @@ sealed class List<out A> {
         fun product2(ints: List<Double>): Double =
             foldLeft(ints, 1.0) { x, y -> x * y }
 
-        tailrec fun <A> startsWith(l1: List<A>, l2: List<A>) : Boolean =
+        tailrec fun <A> startsWith(l1: List<A>, l2: List<A>): Boolean =
             when (l1) {
                 is Nil -> l2 == Nil
-                is Cons -> when(l2) {
+                is Cons -> when (l2) {
                     is Nil -> true
-                    is Cons -> if (l1.head == l2.head)
+                    is Cons -> if (l1.head == l2.head) {
                         startsWith(l1, l2)
-                    else false
+                    } else {
+                        false
+                    }
                 }
             }
 
-        tailrec fun <A> hasSubsequence(xs: List<A>, sub: List<A>) : Boolean =
+        tailrec fun <A> hasSubsequence(xs: List<A>, sub: List<A>): Boolean =
             when (xs) {
                 is Nil -> false
                 is Cons ->
-                    if (startsWith(xs, sub))
+                    if (startsWith(xs, sub)) {
                         true
-                    else hasSubsequence(xs.tail, sub)
+                    } else {
+                        hasSubsequence(xs.tail, sub)
+                    }
             }
     }
 }
